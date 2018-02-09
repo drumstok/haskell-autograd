5498433c1ef2df66c13e11b6194b4dba8f915a91
Pim de Haan <pimdehaan@gmail.com>
2018-02-09 11:47:15 +0100

    Refactord newtype tuple into data

--- simple-ad.hs
+++ simple-ad.hs
@@ -1,63 +1,66 @@
 {-# LANGUAGE Rank2Types #-}
 import Data.Monoid ((<>))
 import Sum (Sum(..))
-import Control.Lens
+import Control.Lens (Lens', view, set, _1, _2)
 
-newtype Node g v = Node { fromNode :: (Node g v -> Node g g, v) }
+data Node g v = Node { gradFn :: Node g v -> Node g g, value :: v }
 
-constant :: (Monoid g, Num v) => v -> Node g v
-constant v = Node (const mempty, v)
+constNode :: Monoid g => v -> Node g v
+constNode = Node mempty
 
 variable :: v -> Node v v
-variable v = Node (id, v)
-
-value :: Node g v -> v
-value = snd . fromNode
+variable = Node id
 
 grad :: (Monoid g, Num v) => Node g v -> Node g g
-grad n = (fst $ fromNode n) (constant 1)
+grad n = gradFn n (constNode 1)
 
-getVar :: Monoid s => Lens' s a -> Node s s -> Node s a
-getVar ln (Node (g, s)) = Node (g . g', view ln s) where
-    g' (Node (gg, gv)) = Node (gg . getVar ln, set ln gv mempty)
+-- Node op wrapper for isomorphisms / bijections
+-- Can not be used for ops that require the forward input for backward
+iso :: (a -> b) -> (b -> a) -> Node g a -> Node g b
+iso fw bw (Node g a) = Node (g . iso bw fw) (fw a)
+
+-- Get node from structure through a lens
+-- Useful for extracting parameters from a variable of a structure of params
+getVar :: Monoid s => Lens' s a -> Node g s -> Node g a
+getVar ln = iso (view ln) (\a -> set ln a mempty)
 
 instance (Monoid g, Monoid v) => Monoid (Node g v) where
-    mempty = Node (const mempty, mempty)
-    Node (gf1, v1) `mappend` Node (gf2, v2) = Node (gf1 <> gf2, v1 <> v2)
+    mempty = constNode mempty
+    Node gf1 v1 `mappend` Node gf2 v2 = Node (gf1 <> gf2) (v1 <> v2)
 
 instance (Monoid g, Num v) => Num (Node g v) where
-    fromInteger = constant . fromInteger
+    fromInteger = constNode . fromInteger
 
-    Node (g1, v1) + Node (g2, v2) = Node (g1 <> g2, v1 + v2)
+    Node g1 v1 + Node g2 v2 = Node (g1 <> g2) (v1 + v2)
 
-    Node (g1, v1) - Node (g2, v2) = Node (g', v1 - v2)
+    Node g1 v1 - Node g2 v2 = Node g' (v1 - v2)
         where g' x = g1 x <> g2 (-x)
 
-    n1@(Node (g1, v1)) * n2@(Node (g2, v2)) = Node (g', v1 * v2)
+    n1@(Node g1 v1) * n2@(Node g2 v2) = Node g' (v1 * v2)
         where g' x = g1 (n2 * x) <> g2 (n1 * x)
 
-    abs n@(Node (g, v)) = Node (g . (* signum n), abs v)
-    signum (Node (_, v)) = constant $ signum v
+    abs n@(Node g v) = Node (g . (* signum n)) (abs v)
+    signum (Node _ v) = constNode $ signum v
 
 instance (Monoid g, Fractional v) => Fractional (Node g v) where
-    fromRational = constant . fromRational
-    recip n@(Node (g, v)) = Node (g . (* (-recip (n * n))), recip v)
+    fromRational = constNode . fromRational
+    recip n@(Node g v) = Node (g . (* (-recip (n * n)))) (recip v)
 
 instance (Monoid g, Floating v) => Floating (Node g v) where
-    pi = constant pi
+    pi = constNode pi
 
-    exp   n@(Node (g, v)) = Node (g . (* exp n)             , exp v)
-    log   n@(Node (g, v)) = Node (g . (* recip n)           , log v)
-    sin   n@(Node (g, v)) = Node (g . (* cos n)             , sin v)
-    cos   n@(Node (g, v)) = Node (g . (* (-sin n))          , cos v)
-    asin  n@(Node (g, v)) = Node (g . (* (1-n*n)**(-0.5))   , asin v)
-    acos  n@(Node (g, v)) = Node (g . (* (-(1-n*n)**(-0.5))), acos v)
-    atan  n@(Node (g, v)) = Node (g . (* recip (1+n*n))     , atan v)
-    sinh  n@(Node (g, v)) = Node (g . (* cosh n)            , sinh v)
-    cosh  n@(Node (g, v)) = Node (g . (* sinh n)            , cosh v)
-    asinh n@(Node (g, v)) = Node (g . (* (1+n*n)**(-0.5))   , asinh v)
-    acosh n@(Node (g, v)) = Node (g . (* (n*n-1)**(-0.5))   , acosh v)
-    atanh n@(Node (g, v)) = Node (g . (* recip (1-n*n))     , atanh v)
+    exp   n@(Node g v) = Node (g . (* exp n))              (exp v)
+    log   n@(Node g v) = Node (g . (* recip n))            (log v)
+    sin   n@(Node g v) = Node (g . (* cos n))              (sin v)
+    cos   n@(Node g v) = Node (g . (* (-sin n)))           (cos v)
+    asin  n@(Node g v) = Node (g . (* (1-n*n)**(-0.5)))    (asin v)
+    acos  n@(Node g v) = Node (g . (* (-(1-n*n)**(-0.5)))) (acos v)
+    atan  n@(Node g v) = Node (g . (* recip (1+n*n)))      (atan v)
+    sinh  n@(Node g v) = Node (g . (* cosh n))             (sinh v)
+    cosh  n@(Node g v) = Node (g . (* sinh n))             (cosh v)
+    asinh n@(Node g v) = Node (g . (* (1+n*n)**(-0.5)))    (asinh v)
+    acosh n@(Node g v) = Node (g . (* (n*n-1)**(-0.5)))    (acosh v)
+    atanh n@(Node g v) = Node (g . (* recip (1-n*n)))      (atanh v)
 
 main :: IO ()
 main = do
